6c697baffad58ee9803938cdf4118907e8868369
Mr Hanzla <mr.hanzla101@gmail.com>
2022-07-21 12:49:43 +0500

    changes to deploy on heroku - part 1

--- application.py
+++ application.py
@@ -1,44 +1,4 @@
-from flask import Flask, render_template, request
-import os
-
-
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flask_app.sqlite')
-    )
-
-    if test_config==None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-    
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    @app.route('/salam')
-    def greet():
-        name = request.args.get('name')
-        print('Name is: {}'.format(name))
-        return render_template('greet.html', name=name)
-    
-    from . import db
-    db.init_app(app)
-
-    from . import auth
-    app.register_blueprint(auth.bp)
-
-    from . import blog
-    app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
-
-    from . import ttt
-    app.register_blueprint(ttt.bp)
-
-    return app
+from flask_app import create_app
 
 if __name__ == '__main__':
     app = create_app()
